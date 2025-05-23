0527c4f11553bb907bdfd3276d296aed065f3890
Berdila17 <l.berdila.inf24@stud.bbbaden.ch>
2025-05-23 11:46:47 +0200

    DbHelper.cs

--- Aufgabenverwalter/DbHelper.cs
+++ Aufgabenverwalter/DbHelper.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
@@ -27,16 +27,17 @@ namespace Aufgabenverwalter
             }
         }
 
-        public static List<(int Id, string Anzeige)> LadeAlleAufgaben()
+        public static List<(int Id, string Anzeige)> LadeAlleAufgaben(int benutzerId)
         {
             var aufgabenListe = new List<(int, string)>();
 
             using (SqlConnection conn = new SqlConnection(connStr))
             {
-                string query = "SELECT id, titel, prioritaet, faellig_am FROM Aufgabe";
+                string query = "SELECT id, titel, prioritaet, faellig_am FROM Aufgabe WHERE benutzer_id = @benutzer_id";
                 SqlCommand cmd = new SqlCommand(query, conn);
-                conn.Open();
+                cmd.Parameters.AddWithValue("@benutzer_id", benutzerId);
 
+                conn.Open();
                 using (SqlDataReader reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
@@ -55,6 +56,7 @@ namespace Aufgabenverwalter
             return aufgabenListe;
         }
 
+
         public static void DeleteTask(int id)
         {
             using (SqlConnection conn = new SqlConnection(connStr))
@@ -66,5 +68,20 @@ namespace Aufgabenverwalter
                 cmd.ExecuteNonQuery();
             }
         }
+        public static void UpdateTask(int id, string neueBeschreibung)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "UPDATE Aufgabe SET titel = @titel, beschreibung = @beschreibung WHERE id = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@titel", neueBeschreibung);
+                cmd.Parameters.AddWithValue("@beschreibung", neueBeschreibung);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
